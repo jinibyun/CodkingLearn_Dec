bcd6e036189d6c601ea778ba41cabc7f61a5c786
Jini Byun <jinibyun@gmail.com>
2019-02-17 16:06:37 -0500

    commit

---
+++ Csharp/Ado.net/testSqlCommand.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.net
+{
+    public class testSqlCommand
+    {
+        SqlConnection conn;
+        public testSqlCommand()
+        {
+            conn = new SqlConnection(
+ "Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI");
+        }
+        public void DoTest()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("Categories Before Insert");
+            Console.WriteLine("------------------------");
+
+            // use ExecuteReader method
+            ReadData();
+
+            // use ExecuteNonQuery method for Insert
+            Insertdata();
+            Console.WriteLine();
+            Console.WriteLine("Categories After Insert");
+            Console.WriteLine("------------------------------");
+
+            ReadData();
+
+            // use ExecuteNonQuery method for Update
+            UpdateData();
+
+            Console.WriteLine();
+            Console.WriteLine("Categories After Update");
+            Console.WriteLine("------------------------------");
+
+            ReadData();
+
+            // use ExecuteNonQuery method for Delete
+            DeleteData();
+
+            Console.WriteLine();
+            Console.WriteLine("Categories After Delete");
+            Console.WriteLine("------------------------------");
+
+            ReadData();
+
+            // use ExecuteScalar method
+            int numberOfRecords = GetNumberOfRecords();
+
+            Console.WriteLine();
+            Console.WriteLine("Number of Records: {0}", numberOfRecords);
+        }
+
+        /// <summary>
+        /// use ExecuteReader method
+        /// </summary>
+        public void ReadData()
+        {
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command with a query and connection
+                SqlCommand cmd = new SqlCommand("select CategoryName from Categories", conn);
+
+                // 2. Call Execute reader to get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CategoryName of each record
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0]);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// use ExecuteNonQuery method for Insert
+        /// </summary>
+        public void Insertdata()
+        {
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // prepare command string
+                string insertString = @"
+                 insert into Categories
+                 (CategoryName, Description)
+                 values ('Miscellaneous', 'Whatever doesn''t fit elsewhere')";
+
+                // 1. Instantiate a new command with a query and connection
+                SqlCommand cmd = new SqlCommand(insertString, conn);
+
+                // 2. Call ExecuteNonQuery to send command
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// use ExecuteNonQuery method for Update
+        /// </summary>
+        public void UpdateData()
+        {
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // prepare command string
+                string updateString = @"
+                update Categories
+                set CategoryName = 'Other'
+                where CategoryName = 'Miscellaneous'";
+
+                // 1. Instantiate a new command with command text only
+                SqlCommand cmd = new SqlCommand(updateString);
+
+                // 2. Set the Connection property
+                cmd.Connection = conn;
+
+                // 3. Call ExecuteNonQuery to send command
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// use ExecuteNonQuery method for Delete
+        /// </summary>
+        public void DeleteData()
+        {
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // prepare command string
+                string deleteString = @"
+                 delete from Categories
+                 where CategoryName = 'Other'";
+
+                // 1. Instantiate a new command
+                SqlCommand cmd = new SqlCommand();
+
+                // 2. Set the CommandText property
+                cmd.CommandText = deleteString;
+
+                // 3. Set the Connection property
+                cmd.Connection = conn;
+
+                // 4. Call ExecuteNonQuery to send command
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// use ExecuteScalar method
+        /// </summary>
+        /// <returns>number of records</returns>
+        public int GetNumberOfRecords()
+        {
+            int count = -1;
+
+            try
+            {
+                // Open the connection
+                conn.Open();
+
+                // 1. Instantiate a new command
+                SqlCommand cmd = new SqlCommand("select count(*) from Categories", conn);
+
+                // 2. Call ExecuteScalar to send command
+                count = (int)cmd.ExecuteScalar();
+            }
+            finally
+            {
+                // Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Csharp/Ado.net/testSqlConnection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ado.net
+{
+    public class testSqlConnection
+    {
+        public void DoTest()
+        {
+            // 1. Instantiate the connection
+            SqlConnection conn = new SqlConnection(
+                "Data Source=(local);Initial Catalog=Northwind;Integrated Security=SSPI");
+
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                // 3. Pass the connection to a command object
+                SqlCommand cmd = new SqlCommand("select * from Customers", conn);
+
+                //
+                // 4. Use the connection
+                //
+
+                // get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CustomerID of each record
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0]);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // 5. Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
