a3fc09b9d347d50c650f7c9dc7d00dce88fcb746
Jini Byun <jinibyun@gmail.com>
2019-03-01 23:13:28 -0500

    commit

---
+++ Csharp/WebApiWithAspnetCore/Module1/Controllers/Product2Controller.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Module1.Data;
+using Module1.Models;
+using Module1.Services;
+
+namespace Module1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Product2Controller : ControllerBase
+    {
+        private IProduct productRepository;
+
+        // check built-in dependency injection on "Startup.cs"
+        public Product2Controller(IProduct _product)
+        {
+            productRepository = _product;
+        }
+
+        // GET: api/Product2
+        // 1. sorting: sortPrice
+        // 2. paging: pageNumber, pageSize
+        // 3. searching: searchProduct
+        [HttpGet]
+        public IEnumerable<Product> Get(int? pageNumber, int? pageSize, string sortPrice, string searchProduct)
+        {
+            IQueryable<Product> products;
+
+            // searching
+            if (!string.IsNullOrEmpty(searchProduct))
+                products = productRepository.GetProducts().Where(x => x.ProductName.StartsWith(searchProduct));
+            else
+                products = productRepository.GetProducts();
+            // sorting
+            switch (sortPrice)
+            {
+                case "desc":
+                    products = products.OrderByDescending(p => p.ProductPrice);
+                    break;
+                case "asc":
+                    products = products.OrderBy(p => p.ProductPrice);
+                    break;
+                //default:
+                //    products = productDbContext.Products;
+                //    break;
+
+            }
+
+            // paging
+            int currentPage = pageNumber ?? 1;
+            int currentPageSize = pageSize ?? 3;
+            var items = products.Skip((currentPage - 1) * currentPageSize).Take(currentPageSize).ToList();
+
+            return items;
+        }
+
+        // GET: api/Product2/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var product = productRepository.GetProduct(id);
+            if (product == null)
+            {
+                return NotFound("No record found...");
+            }
+            return Ok(product);
+        }
+
+        // POST: api/Product2
+        [HttpPost]
+        public IActionResult Post([FromBody] Product value)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            productRepository.AddProduct(value);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT: api/Product2/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product value)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(id != value.ProductId)
+            {
+                return BadRequest();
+            }
+
+            productRepository.UpdateProduct(value);
+
+            return Ok(value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            productRepository.DeleteProduct(id);
+            return Ok("Product Deleted...");
+        }
+    }
+}
---
+++ Csharp/WebApiWithAspnetCore/Module1/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Module1.Data;
+using Module1.Services;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Module1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // default
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // built-in dependency injection
+            services.AddScoped<IProduct, ProductRepository>();
+
+            // versioning
+            // it requires package named "Microsoft.AspNetCore.Mvc.Versioning"
+            services.AddApiVersioning(options => {
+                    options.AssumeDefaultVersionWhenUnspecified = true;
+                    options.ApiVersionReader = new MediaTypeApiVersionReader();
+
+            });            
+
+            // contend negotiation(accept type should be matching with mime type)
+            // to send xml type if client (browser) wants
+            services.AddMvc().AddXmlSerializerFormatters();
+
+            // entiry framework connection string
+            var connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ProductsDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            // var connString = Configuration.GetConnectionString("ProductDbContext"); // azure
+
+            services.AddDbContext<ProductDbContext>(option => option.UseSqlServer(connString));
+
+            // documentation
+            services.AddSwaggerGen(x => x.SwaggerDoc("v1", new Info { Title = "Product Api", Version = "v1" }));
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ProductDbContext productDbContext)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "API for product"));
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            productDbContext.Database.EnsureCreated();
+        }
+    }
+}
